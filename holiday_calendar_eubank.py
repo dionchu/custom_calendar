faefff90edc2a0c145a02a9b3faa340ee23b1428
dionchu <dionchu@gmail.com>
2018-10-15 21:50:33 -0400

    Update holiday_calendar_eubank.py

--- holiday_calendar_eubank.py
+++ holiday_calendar_eubank.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#https://www.ecb.europa.eu/home/contacts/working-hours/html/index.en.html
+    
 from datetime import time
 from pandas import Timestamp
 from pandas.tseries.holiday import (
