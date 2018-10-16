261d7089aa1c82841ee22061a0c0c7873133da96
dionchu <dionchu@gmail.com>
2018-10-16 12:48:59 -0400

    Update holiday_extensions.py

--- holiday_extensions.py
+++ holiday_extensions.py
@@ -1,12 +1,9 @@
 import warnings
-import ephem
 
 from pandas import DateOffset, DatetimeIndex, Series, Timestamp
 from pandas.errors import PerformanceWarning
 from pandas.compat import add_metaclass
 from datetime import datetime, timedelta
-from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU  # noqa
-from pandas.tseries.offsets import Easter, Day
 import numpy as np
 
 def friday_week_of(dt):
@@ -181,44 +178,3 @@ class HolidayWithFilter(object):
                     warnings.simplefilter("ignore", PerformanceWarning)
                     dates += offset
         return dates
-
-    
-def summer_solstice(year):
-    dt = datetime(year,1,1)
-    return ephem.next_summer_solstice(dt).datetime().replace(hour=0, minute=0, second=0, microsecond=0)
-
-
-class SummerSolstice(DateOffset):
-    """
-    DateOffset for the Summer Solstice using
-    logic defined in ephem.
-    """
-    _adjust_dst = True
-    _attributes = frozenset(['n', 'normalize'])
-
-    __init__ = BaseOffset.__init__
-
-    @apply_wraps
-    def apply(self, other):
-        current_summer_solstice = summer_solstice(other.year)
-        current_summer_solstice = conversion.localize_pydatetime(current_summer_solstice,
-                                                        other.tzinfo)
-
-        n = self.n
-        if n >= 0 and other < current_summer_solstice:
-            n -= 1
-        elif n < 0 and other > current_summer_solstice:
-            n += 1
-        # TODO: Why does this handle the 0 case the opposite of others?
-
-        # NOTE: easter returns a datetime.date so we have to convert to type of
-        # other
-        new = easter(other.year + n)
-        new = datetime(new.year, new.month, new.day, other.hour,
-                       other.minute, other.second, other.microsecond)
-        return new
-
-    def onOffset(self, dt):
-        if self.normalize and not _is_normalized(dt):
-            return False
-        return date(dt.year, dt.month, dt.day) == summer_solstice(dt.year)
