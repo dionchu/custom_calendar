135e0de4d06bf23e4ba8bcbe399d42c60a8a5e49
dionchu <dionchu@gmail.com>
2018-10-16 09:21:21 -0400

    Update holiday_calendar_xosl.py

--- holiday_calendar_xosl.py
+++ holiday_calendar_xosl.py
@@ -24,17 +24,66 @@ from pytz import timezone
 
 from .common_holidays import (
     new_years_day,
+    wed_before_maundy_thursday,
+    maundy_thursday,
     european_labour_day,
     ascension_day,
     whit_monday,
-    all_saints_day,    
     christmas_eve,
     christmas,
     boxing_day,
     new_years_eve,
-    summer_solstice_friday,
-    wed_before_maundy_thursday,
-    maundy_thursday,
-    friday_week_of,
 )
 
+NewYearsDay = new_years_day(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY))
+
+WednesdayBeforeMaundyThursday = wed_before_maundy_thursday()
+
+MaundyThursday = maundy_thursday()
+
+EuropeanLabourDay = european_labour_day(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY)
+
+AscensionDay = ascension_day()
+
+NOConstitutionDay = Holiday(
+    "Norwegian Constitution Day",
+    month=5,
+    day=17,
+    days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY),
+)
+
+WhitMonday = whit_monday()
+
+ChristmasEve = christmas_eve(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY))
+
+Christmas = christmas(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY))
+
+BoxingDay = boxing_day(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY))
+
+NewYearsEve = new_years_eve(days_of_week=(MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY))
+
+# Oslo Bors Holiday Calendar
+# -------------------------------------
+
+class XOSL_AbstractHolidayCalendar:
+    
+        regular = HolidayCalendar([
+          NewYearsDay,
+          MaundyThursday,  
+          GoodFriday,
+          EasterMonday,
+          EuropeanLabourDay,
+          AscensionDay,
+          NOConstitutionDay,
+          WhitMonday,
+          ChristmasEve,
+          Christmas,
+          BoxingDay,
+          NewYearsEve,
+        ])
+        
+        regular_adhoc = []
+
+        early = HolidayCalendar([
+          WednesdayBeforeMaundyThursday,
+        ])
