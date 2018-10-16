de5ed36d758aaa1087a5d23ebf016787be469448
dionchu <dionchu@gmail.com>
2018-10-16 15:58:23 -0400

    Update offset_extensions.py

--- extensions/offset_extensions.py
+++ extensions/offset_extensions.py
@@ -13,6 +13,11 @@ def summer_solstice(year):
     dt = datetime(year,1,1)
     return ephem.next_summer_solstice(dt).datetime().replace(hour=0, minute=0, second=0, microsecond=0)
 
+def summer_solstice_friday(year):
+    dt = datetime(year,1,1)
+    dt_solstice = ephem.next_summer_solstice(dt).datetime().replace(hour=0, minute=0, second=0, microsecond=0)
+    return dt_solstice + timedelta(days=4) + timedelta(days=-dt_solstice.weekday())
+
 def localize_pydatetime(dt, tz):
     """
     Take a datetime/Timestamp in UTC and localizes to timezone tz.
@@ -62,7 +67,7 @@ class SummerSolstice(DateOffset):
             n += 1
         # TODO: Why does this handle the 0 case the opposite of others?
 
-        # NOTE: easter returns a datetime.date so we have to convert to type of
+        # NOTE: summer_solstice returns a datetime.date so we have to convert to type of
         # other
         new = summer_solstice(other.year + n)
         new = datetime(new.year, new.month, new.day, other.hour,
@@ -73,3 +78,40 @@ class SummerSolstice(DateOffset):
         if self.normalize and not _is_normalized(dt):
             return False
         return date(dt.year, dt.month, dt.day) == summer_solstice(dt.year)
+
+class SummerSolsticeToFriday(DateOffset):
+    """
+    DateOffset for the Summer Solstice Friday
+    using logic defined in ephem and above
+    """
+    _adjust_dst = True
+    _attributes = frozenset(['n', 'normalize'])
+
+    def __init__(self, n=1, normalize=False):
+        self.n = self._validate_n(n)
+        self.normalize = normalize
+
+    @apply_wraps
+    def apply(self, other):
+        current_summer_solstice_friday = summer_solstice_friday(other.year)
+        current_summer_solstice_friday = localize_pydatetime(current_summer_solstice_friday,
+                                                            other.tzinfo)
+
+        n = self.n
+        if n >= 0 and other < current_summer_solstice_friday:
+            n -= 1
+        elif n < 0 and other > current_summer_solstice_friday:
+            n += 1
+        # TODO: Why does this handle the 0 case the opposite of others?
+
+        # NOTE: summer_solstice returns a datetime.date so we have to convert to type of
+        # other
+        new = summer_solstice_friday(other.year + n)
+        new = datetime(new.year, new.month, new.day, other.hour,
+                       other.minute, other.second, other.microsecond)
+        return new
+
+    def onOffset(self, dt):
+        if self.normalize and not _is_normalized(dt):
+            return False
+        return date(dt.year, dt.month, dt.day) == summer_solstice_friday(dt.year)
