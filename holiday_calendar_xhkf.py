5bc317324f2fbe1d28f9c01a3e3399ac911320dc
Michael Peng <michaelpeng@Michaels-MacBook-Pro.local>
2018-10-16 20:44:14 -0700

    Update holiday_calendar_xhkf.py

--- holiday_calendar_xhkf.py
+++ holiday_calendar_xhkf.py
@@ -8,7 +8,56 @@ from trading_calendars.offset_extensions import (
     next_spring_equinox,
     next_summer_solstice,
 )
+from datetime import time
+from pandas import (
+    Timestamp,
+    DateOffset,
+)
+from pandas.tseries.holiday import (
+    Holiday,
+    DateOffset,
+    MO,
+    TU,
+    WE,
+    TH,
+    FR,
+    SA,
+    SU,
+    nearest_workday,
+    weekend_to_monday,
+    sunday_to_monday,
+    previous_friday,
+    previous_workday,
+    GoodFriday,
+    Easter,
+    EasterMonday
+)
+from pytz import timezone
+# I don't think the below applies for the repo; have commented away and adjusted the otehr imports
+'''
+import sys
 
+import os
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+import trading_calendars
+'''
+from us_holidays import USMemorialDay
+from trading_calendar import (
+    TradingCalendar,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    HolidayCalendar
+)
+from trading_calendar.holiday_extensions import sunday_to_tuesday
+
+# BEGIN LUNAR HOLIDAYS
 SummerSolsticeFriday = HolidayWithFilter(
     'Sweden Summer Solstice Friday',
     month=1,
@@ -74,3 +123,63 @@ DoubleNineFestival = Holiday(
     day=1,
     offset=[next_double_nine_festival(observance=sunday_to_monday)]
 )
+# END LUNAR HOLIDAYS
+# BEGIN NON-LUNAR HOLIDAYS
+
+# New Year's Day: Jan 1, sunday_to_monday
+NewYearsDay = new_years_day(observance=sunday_to_monday)
+
+# Good Friday, Easter, and Easter Monday imported from pandas.
+
+# Labor Day: May 1, sunday_to_monday
+LaborDay = european_labour_day(observance=sunday_to_monday)
+
+# US Memorial Day imported above.
+
+# HK SAR Day: July 1, sunday_to_monday
+HKSARDay = Holiday(
+        'HK SAR Establishment Day',
+        month=7,
+        day=1,
+        start_date='1997-07-01',
+        observance=sunday_to_monday,
+    )
+
+# National Day: Oct. 1, sunday_to_monday
+PRCNationalDay = Holiday(
+        'PRC National Day',
+        month=10,
+        day=1,
+        start_date='1997-07-01',
+        observance=sunday_to_monday,
+)
+
+# Christmas Eve
+ChristmasEve = christmas_eve(days_of_week=(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY))
+
+# Christmas Day
+Christmas = christmas(observance=sunday_to_tuesday)
+
+# Boxing Day
+BoxingDay = boxing_day(observance=sunday_to_monday)
+
+# New Year's Eve
+NewYearsEve = new_years_eve(days_of_week=(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY))
+
+# END NON-LUNAR HOLIDAYS
+# BEGIN AD HOC HOLIDAYS
+
+# 9/3/2015: 70th anniversary of Japanese loss
+HKJapaneseLoss20150903 = [
+    Timestamp('2015-09-03', tz='UTC'), # Should this be UTC?
+]
+
+# 8/23/2017: Typhoon (https://www.hkex.com.hk/news/news-release/2017/1708232news?sc_lang=en)
+Typhoon20170823 = [
+    Timestamp('2017-08-23', tz='UTC'), # Should this be UTC?
+]
+
+# 7/2/1997 Day after HK SAR Day--appears to be one-off statutory holiday for 1997
+HKSAR19970702 = [
+    Timestamp('1997-07-02', tz='UTC'), # Should this be UTC?
+]
